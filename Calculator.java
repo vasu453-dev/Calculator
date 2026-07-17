18e37ace6fb4f1412de77ec808bd62b5d5fdd5ae
Sreenu J <sreenujogi123@gmail.com>
2026-07-17 22:22:30 +0530

    Added Print Statements

--- Calculator.java
+++ Calculator.java
@@ -6,8 +6,13 @@ public class Calculator{
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = addition(a, b);
-        System.out.println("Sum of " + a + " and " + b + " is " + sum);
-
+        int diff = subtraction(a, b);
+        int mul = multiplication(a, b);
+        int div = division(a, b);
+        System.out.println("Addition of" + a + " and " + b + " is " + sum);
+        System.out.println("Difference of " + a + " and " + b + " is " + diff);  
+        System.out.println("Multiplication of " + a + " and " + b + " is " + mul);
+        System.out.println("Division of " + a + " and " + b + " is " + div);
         sc.close();
     }
     public static int addition(int a , int b){

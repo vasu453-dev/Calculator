9f185891477f63dfd5cd51a8be9f5d33686134a3
Sreenu J <sreenujogi123@gmail.com>
2026-07-20 21:55:58 +0530

    Square function added in Calculator

--- Calculator.java
+++ Calculator.java
@@ -9,10 +9,12 @@ public class Calculator{
         int diff = subtraction(a, b);
         int mul = multiplication(a, b);
         int div = division(a, b);
+        int squareA = square(a);
         System.out.println("Addition of" + a + " and " + b + " is " + sum);
         System.out.println("Difference of " + a + " and " + b + " is " + diff);  
         System.out.println("Multiplication of " + a + " and " + b + " is " + mul);
         System.out.println("Division of " + a + " and " + b + " is " + div);
+        System.out.println("Square of "+a+" is "+squareA);
         sc.close();
     }
     public static int addition(int a , int b){
@@ -26,5 +28,8 @@ public class Calculator{
     }
     public static int division(int a , int b){
         return (b == 0) ? 0 : a / b;
+    }
+    public static int square(int a){
+        return a * a;
     }
 }
